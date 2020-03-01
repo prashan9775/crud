5fc7ccc678be9dfd73a95ae3720d4064ccbad60e
https://prashant0608@bitbucket.org <prashant.s@fabhotels.com>
2020-03-01 19:12:51 +0530

    commit

---
+++ build/precache-manifest.652b444159ebc84c74be26bcf610b2ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88dd4e19b536e1976824",
+    "url": "/static/css/main.8f8de70a.chunk.css"
+  },
+  {
+    "revision": "88dd4e19b536e1976824",
+    "url": "/static/js/main.88dd4e19.chunk.js"
+  },
+  {
+    "revision": "6f7aa15d4ec6a40005d4",
+    "url": "/static/js/1.6f7aa15d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0d6187d8c1eda81c453d31db0abb59bd",
+    "url": "/index.html"
+  }
+];
